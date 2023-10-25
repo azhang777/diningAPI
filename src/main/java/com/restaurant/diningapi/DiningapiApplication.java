2386bcebc7b575696440780cf171f335891fe87b
Andy <andyzhanggm@gmail.com>
2023-10-25 00:45:10 -0400

    save changes

---
+++ src/main/java/com/restaurant/diningapi/DiningapiApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.diningapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningapiApplication.class, args);
+	}
+
+}
